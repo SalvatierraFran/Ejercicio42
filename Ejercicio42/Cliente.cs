b78aa73da968f0cacf5e83efe830a553724b97b0
SalvatierraFran <franco14_93@hotmail.com>
2016-05-10 23:17:21 -0300

    Ejercicio de Herencia. Creada Clase Cliente con sus atributos, propiedades y métodos. Creada la clase ClientePlus que hereda de la clase Cliente.

---
+++ Ejercicio42/Cliente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio42
+{
+    class Cliente
+    {
+        /*Atributos*/
+        private string _apellido;
+
+        public string Apellido
+        {
+            get { return _apellido; }
+            set { _apellido = value; }
+        }
+        private int _cuentaBancaria;
+        private int _cuit;
+        private int _edad;
+        private string _nombre;
+
+        /*Propiedades*/
+        
+
+        /*Métodos*/
+        public void GuardarEnDisco()
+        { }
+
+        public void ValidarCUIT()
+        { }
+    }
+}
